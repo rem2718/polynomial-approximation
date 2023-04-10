99336586578628ef64df041ceec98b668d7a291d
reme2.718 <103392516+rem2718@users.noreply.github.com>
2023-04-10 15:10:36 +0300

    a

--- newton_backward.m
+++ newton_backward.m
@@ -1,4 +1,4 @@
-function L = newton_forward(x, y)
+function L = newton_backward(x, y)
     % compute the number of points
     n = length(x);
     h = x(2) - x(1);
@@ -7,17 +7,18 @@ function L = newton_forward(x, y)
     fd_table(:,1) = y';
     for i = 2:n
         for j = 1:n-i+1
-            %fprintf('j: %d val: %f %f %f %f\n',j, );
             fd_table(j,i) = (fd_table(j+1,i-1) - fd_table(j,i-1)) / (h*(i-1));
         end
     end
     %define the Newton forward difference function
     syms t;
-    f = fd_table(1,1);
+    s = (t - x(n))/h;
+    f = fd_table(n,1);
     for i = 2:n
-        term = fd_table(1,i);
-        for j = 1:i-1
-            term = term * (t-x(j));
+        %fprintf("i: %d val: %d\n", i, n-i+1);
+        term = fd_table(n-i+1,i);
+        for j = 0:i-2
+            term = term * (s+j) * h;
         end
         f = f + term;
     end
