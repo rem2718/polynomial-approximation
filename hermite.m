a6ef5d0265be4bf7f29ab769fe7d059caa1d4f56
reme2.718 <103392516+rem2718@users.noreply.github.com>
2023-04-13 06:27:43 +0300

    done

--- hermite.m
+++ hermite.m
@@ -1,26 +1,26 @@
 function L = hermite(x, y, dy)
-    % compute the number of points
     n = length(x);
-    h = x(2) - x(1);
     delta = zeros(n*2);
     k = 1;
+    z = zeros(1,n*2);
     for i = 1:n
         delta(k,1) = y(i);
         delta(k+1,1) = y(i);
         delta(k,2) = dy(i);
+        z(k) = x(i);
+        z(k+1) = x(i);
         k = k + 2;    
     end
     for i = 2:n*2
+        p = i;
         for j = 1:(n*2)-i+1
+            q = j;
             if i == 2 && mod(j,2) ~= 0
+                p = p + 1;
                 continue;
             end
-            ind = floor((i-1)/2);
-            if ind == 0
-                ind = 1;
-            end
-            fprintf('%d',ind);
-            delta(j,i) = (delta(j+1,i-1) - delta(j,i-1)) / (h*ind);
+            delta(j,i) = (delta(j+1,i-1) - delta(j,i-1)) / (z(p)-z(q));
+            p = p + 1;
         end
     end
     syms t;
